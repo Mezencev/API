5454a4d8b11f873159e49f6737ae80dbe754c558
mezencev <mezencev20@rambler.ru>
2017-12-12 07:15:37 +0200

    lab_2

--- lesson.js
+++ lesson.js
@@ -1,5 +1,5 @@
 // Lab_1
-const http = require('http');
+/*const http = require('http');
 const fs = require('fs');
 let file = fs.readFileSync('index.html');
 
@@ -9,4 +9,30 @@ let app = http.createServer((req,res) => {
     res.end();
     
 }).listen(8080);
-console.log('listening on port 8080');
+console.log('listening on port 8080');*/
+
+//Lab_2
+const http = require('http');
+const add = http.createServer().listen(8080);
+add.on('request', (req, res)=>{
+    if(req.url == '/stop'){
+        add.close();
+    }
+    res.writeHead(200);
+    res.write('Hello from Node.js');
+    res.end();
+})
+add.on('request', (req, res)=>{
+    console.log(req.method, req.url, res.statusCode);
+})
+
+add.on('connection', (req, res)=>{
+    console.log('Connect..');
+})
+add.on('close', (req, res)=>{
+    console.log('The end');
+})
+
+add.on('listening', (req, res)=>{
+    console.log('listen: port 8080..');
+})
